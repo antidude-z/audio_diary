f7bc41fc009beb31cb617bd8a180edb58e0c3c85
GlebRaf <RafikovGleb@yandex.com>
2024-11-08 18:00:24 +0500

    del_note
    добавил возможность удалять записи из таблицы , вводя имя записи

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ def main():
     user_text = (req['request']['original_utterance']).lower()
     funk_1 = 'create_note'
     funk_2 = 'create_name'
+    funk_3 = 'delete_note'
 
     conn = psycopg2.connect(dbname='my_users', user='postgres',        #соединяюсь с бд
                         password='2G5i7r1a6f3E', host='localhost')
@@ -22,11 +23,10 @@ def main():
     cursor.execute("SELECT * FROM user_states WHERE user_id = %s", (user_id,))  #получаю строки, в которых указан id юзера
     records = cursor.fetchall()
 
-    if len(records) != 2:  #проверяю есть ли у юзера вообще поля - если нет, добавляю их
+    if len(records) != 3:  #проверяю есть ли у юзера вообще поля - если нет, добавляю их
         cursor.execute("INSERT INTO user_states (user_id, funk, state) VALUES (%s, %s, %s)", (user_id, funk_1, False))
-        conn.commit()
         cursor.execute("INSERT INTO user_states (user_id, funk, state) VALUES (%s, %s, %s)", (user_id, funk_2, False))
-        conn.commit()
+        cursor.execute("INSERT INTO user_states (user_id, funk, state) VALUES (%s, %s, %s)", (user_id, funk_3, False))
 
     if req['session']['new']:  #проверяю сессию: новая -> True, нет -> False
         response_text = 'Начало работы тестового навыка'
@@ -34,32 +34,35 @@ def main():
         if intent_id.get('new_note') != None:  #обрабатываю запрос пользователя
             response_text = 'Придумайте имя записи'
             cursor.execute("UPDATE user_states SET state = %s WHERE user_id = %s AND funk = %s", (True, user_id, funk_2))  #state = True (в бд)
-            conn.commit()
         elif intent_id.get('del_note') != None:  #обрабатываю другой вариант запроса пользователя
-            response_text = 'Запись успешно удалена'
+            response_text = 'Запись, с каким названием, Вы бы хотели удалить?'
+            cursor.execute("UPDATE user_states SET state = %s WHERE user_id = %s AND funk = %s", (True, user_id, funk_3))
         else:
             cursor.execute("SELECT funk, state FROM user_states WHERE user_id = %s", (user_id,))  #вытаскиваю из бд значение поля state
             for row in cursor.fetchall():
                 if row == ('create_name', True):  #проверяю нужно ли создавать имя записи
                     cursor.execute("INSERT INTO users (note_date, note_name, user_id) VALUES (%s, %s, %s)", (date_today, user_text, user_id))
-                    conn.commit()
-                    response_text = 'Имя заметки сохранено. Начало записи'
+                    response_text = 'Имя заметки сохранено. Начало записи...'
                     cursor.execute("UPDATE user_states SET state = %s WHERE user_id = %s AND funk = %s", (False, user_id, funk_2))
-                    conn.commit()
                     cursor.execute("UPDATE user_states SET state = %s WHERE user_id = %s AND funk = %s", (True, user_id, funk_1))
-                    conn.commit()
                 elif row == ('create_note', True):  #проверяю нужно ли создавать запись
                     cursor.execute("SELECT note_name FROM users WHERE user_id = %s AND note_date = %s", (user_id, date_today))
                     records = cursor.fetchall()
                     for i in records[-1]:
                         name = i
                     cursor.execute("UPDATE users SET full_note = %s WHERE note_name = %s", (user_text, name))
-                    conn.commit()
-                    response_text = 'Новая запись успешно добавлена'
+                    response_text = 'Новая запись успешно добавлена!'
                     cursor.execute("UPDATE user_states SET state = %s WHERE user_id = %s AND funk = %s", (False, user_id, funk_1))
-                    conn.commit()
+                elif row == ('delete_note', True):
+                    cursor.execute("SELECT * FROM users WHERE note_name = %s AND user_id = %s", (user_text, user_id))
+                    if len(cursor.fetchall()) > 0:
+                        cursor.execute("DELETE FROM users WHERE note_name = %s AND user_id = %s", (user_text, user_id))
+                        response_text = 'Запись успешно удалена!'
+                    else:
+                        response_text = 'У вас нет записи с таким названием'
+                    cursor.execute("UPDATE user_states SET state = %s WHERE user_id = %s AND funk = %s", (False, user_id, funk_3))
                 else:
-                    response_text = 'запрос не распознан'
+                    response_text = 'Запрос не распознан'
     
     response = {
         'version': req['version'],
@@ -70,6 +73,7 @@ def main():
             },
     }
 
+    conn.commit() #сохраняю изменения
     cursor.close()  #закрываю курсор и соединение
     conn.close()
     return response
